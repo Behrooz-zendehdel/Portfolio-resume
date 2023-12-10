f6c7b20b040d7fe444bdcdc9c8ca971e436466c3
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-10 22:39:40 +0330

    reset code css and import google font Urbanist

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+import Aos from "aos";
 function App() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
   return <h1 className="text-indigo-600 text-2xl font-[500]">hello world</h1>;
 }
 
