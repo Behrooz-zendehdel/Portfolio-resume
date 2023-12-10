b5fb576950280b13d5b9c34ac879e7719810a8e3
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-10 23:20:10 +0330

    change detail padding and css tailwind

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
         <div className="flex items-center justify-between">
           {/* =========logo=========== */}
           <div className="flex items-center gap-[10px]">
-            <span className="w-[25px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
+            <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
               B
             </span>
             <div className="leading-[20px]">
               <h2 className="text-xl text-smallTextColor font-[700]">
                 Behrooz
               </h2>
-              <p className="text-smallTextColor text-[18px] font-[500]">
+              <p className="text-smallTextColor text-[14px] font-[500]">
                 Personal
               </p>
             </div>
