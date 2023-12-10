f156a1e756c0cac538dfb7353ef71c78fc4cca4b
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-10 22:26:46 +0330

    debug tailwind css and fix error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-
-import './App.css'
-
 function App() {
-
-  return (
-    
-      <div></div>
-  )
+  return <h1 className="text-indigo-600 text-2xl font-[500]">hello world</h1>;
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 // #
 export default {
-  //support file tailwind 
+  //support file tailwind
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       //set color basic project
-      colors:{
-        primaryColor:'#8873ef',
-        headingColor:'#081e21',
-        smallTextColor:'#193256',
-      }
+      colors: {
+        primaryColor: "#8873ef",
+        headingColor: "#081e21",
+        smallTextColor: "#193256",
+      },
     },
   },
   plugins: [],
-}
-
+};
