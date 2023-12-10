09a56531cad9d860d763add5dc6b165a91c8df20
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-10 21:43:33 +0330

    set color basic project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+// #
 export default {
   //support file tailwind 
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      //set color basic project
+      colors:{
+        primaryColor:'#8873ef',
+        headingColor:'#081e21',
+        smallTextColor:'#193256',
+      }
+    },
   },
   plugins: [],
 }
