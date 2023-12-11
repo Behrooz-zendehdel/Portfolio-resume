c2f6b9a31c3bf1d2147e901ff08b942f8415fe39
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-11 22:51:16 +0330

    add menu - debug logo section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-
 const Header = () => {
   return (
-    <header className="w-full h-[80px] leading-[80px] flex items-center">
+    <header className="w-full h-[80px] leading-[80px] flex justify-center items-center">
+      {/* check debug justify-center */}
       <div className="container">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between ">
           {/* =========logo=========== */}
           <div className="flex items-center gap-[10px]">
             <span className="w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
@@ -20,6 +19,39 @@ const Header = () => {
             </div>
           </div>
           {/* ===========logo end================ */}
+          {/* =============== menu start============ */}
+          <div className="menu">
+            <ul className="flex items-center gap-10">
+              <li>
+                <a className="text-smallTextColor font-[600]" href="#about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a className="text-smallTextColor font-[600]" href="#services">
+                  Services
+                </a>
+              </li>
+              <li>
+                <a className="text-smallTextColor font-[600]" href="#portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li>
+                <a className="text-smallTextColor font-[600]" href="#contant">
+                  Contant
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* ===============menu end ============= */}
+          {/* ====================menu right start ============= */}
+          <div className=" flex items-center gap-4">
+            <button className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] ease-out duration-700">
+              <i className="ri-send-plane-line"></i>Lets Talk
+            </button>
+          </div>
+          {/* ====================menu right end ============= */}
         </div>
       </div>
     </header>
