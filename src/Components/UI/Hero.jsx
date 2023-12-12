a497deb7d6a320dd63c9d0daec7e654e3b6ae2d8
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-12 23:09:07 +0330

    add data profile and social media

--- src/Components/UI/Hero.jsx
+++ src/Components/UI/Hero.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import heroImg from "../../assets/images/hero.svg";
 import Countup from "react-countup";
+import { CiMail } from "react-icons/ci";
+import { IoLogoAppleAr } from "react-icons/io5";
+import { CiYoutube, CiLinkedin } from "react-icons/ci";
+import { FaWhatsapp, FaInstagram } from "react-icons/fa";
+
 import "aos/dist/aos.css";
 const Hero = () => {
   return (
     <section className="pt-0" id="#about">
-      <div className="container pt-14">
-        <div className="md:flex items-center justify-between sm:flex-col md:flex-row">
+      <div className="container pt-14 ">
+        <div className="md:flex flex  items-center justify-between sm:flex-col md:flex-row">
           {/* =========== hero left contant ============== */}
-          <div className="w-full md:basis-1/2">
+          <div className="w-full flex flex-col justify-between items-center md:basis-1/2">
             <h5
               data-aos="fade-down"
               data-aos-duration="1500"
@@ -16,6 +21,7 @@ const Hero = () => {
             >
               Hello wellcome
             </h5>
+            {/* consume profile name and family */}
             <h1
               data-aos="fade-left"
               data-aos-duration="1500"
@@ -23,6 +29,93 @@ const Hero = () => {
             >
               I m Behrooz zendedel <br /> Frontend Developer
             </h1>
+
+            <div
+              className="flex items-center gap-6 mt-7"
+              data-aos="fade-up"
+              data-aos-duration="1800"
+              data-aos-delay="200"
+            >
+              <a href="#contant">
+                <button className="flex items-center bg-primaryColor text-white font-[500] ap-2 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px]">
+                  <CiMail size={20} />
+                  Hire me
+                </button>
+              </a>
+              {/* see portfolio and follow me  */}
+              <a
+                href="#portfolio"
+                className="text-smallTextColor font-[600] text-[16px border-b border-smallTextColor"
+              >
+                see portfolio
+              </a>
+            </div>
+            {/* description live me  */}
+            <p
+              className="flex gap-2  text-smallTextColor mt-12 font-[500] text-[15px] leading-7 sm:pr-10 sm:justify-center text-center w-full"
+              data-aos="fade-left"
+              data-aos-duration="1500"
+            >
+              <span className="mt-2">
+                <IoLogoAppleAr />
+              </span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Laudantium, amet!
+            </p>
+            <div className="flex items-center gap-9 mt-14">
+              <span className="text-smallTextColor text-[15px] font-[500]">
+                Follow me
+              </span>
+              {/* social media and follow me  */}
+              <span>
+                <a
+                  href="#youtube"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <CiYoutube
+                    size={30}
+                    data-aos="fade-up"
+                    data-aos-duration="1200"
+                  />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#youtube"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <FaWhatsapp
+                    size={30}
+                    data-aos="fade-down"
+                    data-aos-duration="1200"
+                  />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#youtube"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <FaInstagram
+                    size={30}
+                    data-aos="fade-up"
+                    data-aos-duration="1200"
+                  />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="#youtube"
+                  className="text-smallTextColor text-[18px] font-[600]"
+                >
+                  <CiLinkedin
+                    size={30}
+                    data-aos="fade-down"
+                    data-aos-duration="1200"
+                  />
+                </a>
+              </span>
+            </div>
           </div>
           {/* =========== hero left end ============== */}
         </div>
