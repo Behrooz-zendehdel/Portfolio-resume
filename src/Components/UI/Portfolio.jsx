a7421fbea4e77c950cb2a0b0dfea8ee907974fc9
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-15 19:14:00 +0330

    edit text card

--- src/Components/UI/Portfolio.jsx
+++ src/Components/UI/Portfolio.jsx
@@ -29,15 +29,15 @@ const Portfolio = () => {
               data-aos-delay="50"
               data-aos-duration="1200"
               key={index}
-              className="group max-w-full w-64 h-48 relative z-[1]"
+              className="group max-w-full w-64 h-48 relative z-[1] mt-12"
             >
               <figure>
                 <img className="rounded-[8px]" src={portfolio.imgUrl} alt="" />
               </figure>
-              <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-[5] hidden group-hover:block ">
+              <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute mt-4 top-0 left-0 z-[5] hidden group-hover:block ">
                 <div className="w-full h-full flex items-center justify-center">
                   <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-4 rounded-[8px] font-[500] ease-in duration-200 ">
-                    See Detail
+                    Comming soong
                   </button>
                 </div>
               </div>
