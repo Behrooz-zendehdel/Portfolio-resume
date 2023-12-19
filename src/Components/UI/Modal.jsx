a4d4b6666988a6dd2bfaa8fd8e12d1cc3a5674af
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-19 21:46:34 +0330

    edit view image mt and resize for click image

--- src/Components/UI/Modal.jsx
+++ src/Components/UI/Modal.jsx
@@ -3,8 +3,14 @@ import portfolios from "../../assets/data/portfolioData";
 const Modal = ({ activeId, setShowModal }) => {
   const portfolio = portfolios.find((portfolio) => portfolio.id === activeId);
   return (
-    <div>
-      <img src={portfolio.imgUrl} alt="" className="mt-4" />
+    <div className="w-full h-full fixed top-0 left-0 z-10 bg-headingColor bg-opacity-40">
+      <div className="max-w-[600px] absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] p- transform -translate-x-1/2 -translate-y-1/2">
+        <div>
+          <figure>
+            <img src={portfolio.imgUrl} alt="" className="" />
+          </figure>
+        </div>
+      </div>
     </div>
   );
 };
