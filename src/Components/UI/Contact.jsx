988febb3777642ee5ea274990b98a865f703c84b
behrooz <behrooz.zendedel1992@gmail.com>
2023-12-20 20:39:35 +0330

    edit code for view flex

--- src/Components/UI/Contact.jsx
+++ src/Components/UI/Contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <section id="contact" className="pb-16">
       <div className="container">
-        <h2 className="text-headingColor font-[700] text-[2.5rem] mb-8">
+        <h2 className="text-headingColor font-[700] items-center justify-center flex text-[2.5rem] mb-8 mt-12">
           {" "}
           Get in touch
         </h2>
@@ -15,42 +15,43 @@ const Contact = () => {
               className="border-0 w-full h-full "
             ></iframe>
           </div>
-        </div>
-        <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8">
-          <form action="" className="w-full">
-            <div className="mb-5">
-              <input
-                type="text"
-                placeholder="Enter your name"
-                className="w-full p-3 focus:outline-none rounded-[5px]"
-              />
-            </div>
-            <div className="mb-5">
-              <input
-                type="text"
-                placeholder="Enter your email"
-                className="w-full p-3 focus:outline-none rounded-[5px]"
-              />
-            </div>
-            <div className="mb-5">
-              <input
-                type="text"
-                placeholder="Subject"
-                className="w-full p-3 focus:outline-none rounded-[5px]"
-              />
-            </div>
-            <div className="mb-5">
-              <textarea
-                type="text"
-                rows={3}
-                placeholder="Write your message"
-                className="w-full p-3 focus:outline-none rounded-[5px]"
-              />
-            </div>
-            <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
-              Send Message
-            </button>
-          </form>
+
+          <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8 py-8">
+            <form action="" className="w-full">
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Enter your email"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Subject"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <div className="mb-5">
+                <textarea
+                  type="text"
+                  rows={3}
+                  placeholder="Write your message"
+                  className="w-full p-3 focus:outline-none rounded-[5px]"
+                />
+              </div>
+              <button className="w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150">
+                Send Message
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </section>
